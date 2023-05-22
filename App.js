520a2a25641c319ba495ca093456727c53473eb8
alirazahb <aliraza.ar7785@gmail.com>
2023-05-22 21:54:42 +0500

    Adding up firebase

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
-import StackNavigator from "../StackNavigator";
-import store from "../store";
+import StackNavigator from "./StackNavigator";
+import store from "./store";
 
 export default function App() {
   return (
@@ -14,9 +12,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
